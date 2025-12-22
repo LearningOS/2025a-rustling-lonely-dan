f7b1036fe5813e467498a84af829ac4fdb1f6920
lonely-dan <3216418156@qq.com>
2025-12-22 15:13:09 +0000

    modules2

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,18 +1,9 @@
-// modules2.rs
-//
-// You can bring module paths into scopes and provide new names for them with
-// the 'use' and 'as' keywords. Fix these 'use' statements to make the code
-// compile.
-//
-// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod delicious_snacks {
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // 1. 使用 pub 让别名对外可见
+    // 2. 用 as 重命名 PEAR 为 fruit，CUCUMBER 为 veggie
+    // 3. 补充分号完成语法
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
