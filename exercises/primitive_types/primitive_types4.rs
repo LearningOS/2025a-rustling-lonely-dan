33098a945e6656ae11b848dd9f71d255ecdeeeea
lonely-dan <3216418156@qq.com>
2025-12-22 15:01:20 +0000

    primitive4

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -1,15 +1,8 @@
-// primitive_types4.rs
-//
-// Get a slice out of Array a where the ??? is so that the test passes.
-//
-// Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
-// for a hint.
-
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
+    // 截取从索引 1 到 4 的切片（包含 1，不包含 4），对应元素 2、3、4
     let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
