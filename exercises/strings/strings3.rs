893f45f0399030a09b9345c0e6eff347b8acd7cc
lonely-dan <3216418156@qq.com>
2025-12-22 14:51:37 +0000

    strings3

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,23 +1,24 @@
-// strings3.rs
-//
-// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    // 去除首尾空白，再转换为 String
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // 方法1：使用 format! 宏（推荐）
+    format!("{} world!", input)
+
+    // 方法2：使用 + 运算符（也可实现）
+    // input.to_string() + " world!"
+
+    // 方法3：使用 push_str 方法（也可实现）
+    // let mut s = input.to_string();
+    // s.push_str(" world!");
+    // s
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // 替换所有 "cars" 为 "balloons"
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
