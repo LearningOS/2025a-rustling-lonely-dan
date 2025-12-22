4aa12b80df26a5db58c9fbc461060a0341988901
lonely-dan <3216418156@qq.com>
2025-12-22 15:17:02 +0000

    macros3

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -1,12 +1,5 @@
-// macros3.rs
-//
-// Make me compile, without taking the macro out of the module!
-//
-// Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// 在模块上添加 #[macro_use]，导出模块内的宏
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
