ce9eff73fc27f6dfd3c82ec769d123e80ec40035
lonely-dan <3216418156@qq.com>
2025-12-22 15:05:24 +0000

    options3

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,10 +1,3 @@
-// options3.rs
-//
-// Execute `rustlings hint options3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -14,8 +7,9 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // 使用 ref 关键字，借用 p 的值，不转移所有权
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+    y; // 此时 y 仍有所有权，可以正常使用
 }
