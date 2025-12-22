116741a700ad34d0e78639eedbd0fac75966ecef
lonely-dan <3216418156@qq.com>
2025-12-22 16:05:52 +0000

    test2

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -1,17 +1,11 @@
-// tests2.rs
-//
-// This test has a problem with it -- make the test compile! Make the test pass!
-// Make the test fail!
-//
-// Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        // 传入两个相等的值，断言成功，测试通过
+        assert_eq!(1 + 1, 2);
+        // 也可以传入字符串、其他数值类型等
+        // assert_eq!("hello", "hello");
+        // assert_eq!(5, 5);
     }
 }
