e9c0694d3332298e4f119959bda7e9f08c898e43
lonely-dan <3216418156@qq.com>
2025-12-22 15:00:43 +0000

    primitive3

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -1,13 +1,10 @@
-// primitive_types3.rs
-//
-// Create an array with at least 100 elements in it where the ??? is.
-//
-// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
-    let a = [0; 1000];
+    // 方式1：使用 [0; 100] 创建长度为 100 的数组（元素全为 0）
+    let a = [0; 100];
+
+    // 也可以用其他长度（如 101、200、1000 等），只要 >=100 即可
+    // let a = [1; 150];
+    // let a = ["hello"; 200];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
