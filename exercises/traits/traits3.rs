b27c15980acecffd9e9b03d7ac14e7e6d8b436ee
lonely-dan <3216418156@qq.com>
2025-12-22 16:20:30 +0000

    traits3

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -1,17 +1,8 @@
-// traits3.rs
-//
-// Your task is to implement the Licensed trait for both structures and have
-// them return the same information without writing the same function twice.
-//
-// Consider what you can add to the Licensed trait.
-//
-// Execute `rustlings hint traits3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    // 为特质添加默认实现，返回固定的许可信息
+    fn licensing_info(&self) -> String {
+        String::from("Some information")
+    }
 }
 
 struct SomeSoftware {
@@ -22,6 +13,7 @@ struct OtherSoftware {
     version_number: String,
 }
 
+// 空实现即可继承特质的默认方法
 impl Licensed for SomeSoftware {} // Don't edit this line
 impl Licensed for OtherSoftware {} // Don't edit this line
 
