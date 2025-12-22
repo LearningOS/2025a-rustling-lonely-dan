ce1fbc2c2561eefe8e505c5c32b429e698d58fe0
lonely-dan <3216418156@qq.com>
2025-12-22 15:12:23 +0000

    modules1

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,17 +1,11 @@
-// modules1.rs
-//
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod sausage_factory {
-    // Don't let anybody outside of this module see this!
+    // 保持私有，仅模块内可访问
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // 声明为公共函数，允许模块外访问
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
