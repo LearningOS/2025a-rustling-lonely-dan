fbe8609f105bbb44a85933f8d8fbf191eaa58efc
lonely-dan <3216418156@qq.com>
2025-12-22 15:16:19 +0000

    macros2

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,16 +1,10 @@
-// macros2.rs
-//
-// Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
-fn main() {
-    my_macro!();
-}
-
+// 宏定义在使用之前，编译器可以识别
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
     };
+}
+
+fn main() {
+    my_macro!();
 }
